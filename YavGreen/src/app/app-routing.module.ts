ae75b5d149683df4e0e1f11b0fd609d1968805d5
BX41FORCE <bx41force@gmail.com>
2019-12-30 20:08:31 -0500

    Prototipos de pantallas (Maquetas)

--- YavGreen/src/app/app-routing.module.ts
+++ YavGreen/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { RegistroComponent } from './registro/registro.component';
+import { CrearNoticiaComponent} from'./crear-noticia/crear-noticia.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'crear_noticia', component: CrearNoticiaComponent },
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- YavGreen/src/app/app.module.ts
+++ YavGreen/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { RegistroComponent } from './registro/registro.component';
+import { CrearNoticiaComponent } from './crear-noticia/crear-noticia.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    InicioComponent,
+    RegistroComponent,
+    CrearNoticiaComponent
   ],
   imports: [
     BrowserModule,
