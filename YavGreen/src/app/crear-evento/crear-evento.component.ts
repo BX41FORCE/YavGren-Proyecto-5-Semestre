46e2d0a0a9f692dd4d3651fff8565efba2696f16
BX41FORCE <bx41force@gmail.com>
2020-01-07 13:29:25 -0500

    Adición de Codigo

--- YavGreen/src/app/crear-evento/crear-evento.component.ts
+++ YavGreen/src/app/crear-evento/crear-evento.component.ts
@@ -1,35 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-crear-evento',
-  templateUrl: './crear-evento.component.html',
-  styleUrls: ['./crear-evento.component.css']
+  selector: "app-crear-evento",
+  templateUrl: "./crear-evento.component.html",
+  styleUrls: ["./crear-evento.component.css"]
 })
 export class CrearEventoComponent implements OnInit {
-  titulo : string;
-  qrcodename : number;
-  title = 'generate-qrcode';
-  elementType: 'url' | 'canvas' | 'img' = 'url';
+  titulo: string;
+  qrcodename: number;
+  title = "generate-qrcode";
+  elementType: "url" | "canvas" | "img" = "url";
   value: string;
   display = false;
-  href : string;
-  generateQRCode(){
-    if(this.qrcodename == null || this.titulo == null){
+  href: string;
+  codigo: string;
+  generateQRCode() {
+    var abecedario = [
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+      "K",
+      "L",
+      "M",
+      "N",
+      "O",
+      "P",
+      "Q",
+      "R",
+      "S",
+      "T",
+      "U",
+      "V",
+      "W",
+      "X",
+      "Y",
+      "Z"
+    ];
+    var str = '';
+    for (var i = 1; i <= 5; i++) {
+      str +=abecedario[Math.round(Math.random() * (25 - 0) + 0)]+Math.round(Math.random()* (3 - 1) + 1);
+    }
+    document.getElementById('codigo').innerHTML=str;
+    alert(str);
+    if (this.qrcodename == null || this.titulo == null) {
       alert("Los campos deben estar llenos");
       this.display = false;
       return;
-    }
-    else{
-      this.value = this.titulo+"\n"+this.qrcodename.toString();
+    } else {
+      this.value = this.titulo + "\n" + this.qrcodename.toString();
       this.display = true;
     }
   }
-  downloadImage(){
-    this.href = document.getElementsByTagName('img')[0].src;
+  downloadImage() {
+    this.href = document.getElementsByTagName("img")[0].src;
   }
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
