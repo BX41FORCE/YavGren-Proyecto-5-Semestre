5797502485de50d34d1c307b6306e89acebeeed7
BX41FORCE <bx41force@gmail.com>
2020-01-20 13:42:10 -0500

    Modificación de Creación de Noticia y Eventos

--- YavGreen/src/app/crear-noticia/crear-noticia.component.ts
+++ YavGreen/src/app/crear-noticia/crear-noticia.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-crear-noticia',
-  templateUrl: './crear-noticia.component.html',
-  styleUrls: ['./crear-noticia.component.css']
+  selector: "app-crear-noticia",
+  templateUrl: "./crear-noticia.component.html",
+  styleUrls: ["./crear-noticia.component.css"]
 })
 export class CrearNoticiaComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  public imagePath;
+  imgURL: any;
+  public message: string;
+
+  preview(files) {
+    if (files.length === 0) return;
+
+    var mimeType = files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      alert("Solo debe subir archivos de imagen");
+      return;
+    }
+
+    var reader = new FileReader();
+    this.imagePath = files;
+    reader.readAsDataURL(files[0]);
+    reader.onload = _event => {
+      this.imgURL = reader.result;
+    };
   }
-
 }
