145740e47e531e33a5f2cb61eb45dd801d22eac2
BX41FORCE <bx41force@gmail.com>
2020-01-20 23:03:04 -0500

    Creación de Lectura de QR

--- YavGreenMovil/src/app/lector-qr/lector-qr.module.ts
+++ YavGreenMovil/src/app/lector-qr/lector-qr.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
-
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { LectorQrPageRoutingModule } from './lector-qr-routing.module';
-
 import { LectorQrPage } from './lector-qr.page';
+
+import { NgxQRCodeModule } from 'ngx-qrcode2';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    LectorQrPageRoutingModule
+    LectorQrPageRoutingModule,
+    NgxQRCodeModule
   ],
-  declarations: [LectorQrPage]
+  declarations: [LectorQrPage],
 })
-export class LectorQrPageModule {}
+export class LectorQrPageModule { }
--- YavGreenMovil/src/app/lector-qr/lector-qr.page.ts
+++ YavGreenMovil/src/app/lector-qr/lector-qr.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-lector-qr',
@@ -6,10 +8,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lector-qr.page.scss'],
 })
 export class LectorQrPage implements OnInit {
+  qrData = 'https://ionicacademy.com/';
+  scannedCode = null;
+  elementType: 'url' | 'canvas' | 'img' = 'canvas';
+  constructor(private barcodeScanner: BarcodeScanner, private toastCtrl: ToastController) {
 
-  constructor() { }
+  }
+
+  scanCode() {
+    this.barcodeScanner.scan().then(
+      barcodeData => {
+        this.scannedCode = barcodeData.text;
+      }
+    )
+  }
+
+  codificar() {
+    alert("Felicidades se acreditaron tus 100 puntos")
+    var nombre = "";
+    var puntaje = "";
+    var codigo = "";
+    var contador = 0;
+    for (var i = 0; i < this.scannedCode.toString().length; i++) {
+      var caracter = this.scannedCode.toString().charAt(i);
+      if (caracter == '\n') {
+        contador++
+      }
+      if (contador == 0 && caracter != '\n') {
+        nombre += caracter;
+      } else if (contador == 1 && caracter != '\n') {
+        puntaje += caracter
+      } else if (contador == 2 && caracter != '\n') {
+        codigo += caracter
+      }
+    }
+    alert(nombre);
+    alert(puntaje);
+    alert(codigo);
+  }
 
   ngOnInit() {
   }
-
 }
