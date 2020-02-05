479b32fc08f11c4945263f13adbca08bc37632c1
Diego Mauricio Encalada Aguilar <dma.encalada@yavirac.edu.ec>
2020-02-05 12:12:38 -0500

    muchas coas echas que no tengo ni idea

--- YavGreen/src/app/lista/lista.component.ts
+++ YavGreen/src/app/lista/lista.component.ts
@@ -12,11 +12,12 @@ export class ListaComponent implements OnInit {
     titulo:null,
     descripcion:null,
     img : null ,
+    costo:null,
   }
   
-  producto = [{numero:'1',titulo:'Producto 1', descripcion:'Descripiòn del producto',img:'assets/img/producto1.jpg'},
-  {numero:'2',titulo:'Producto 2', descripcion:'Descripiòn del producto', img:'assets/img/producto2.jpg'},
-  {numero:'3',titulo:'Producto 3', descripcion:'Descripiòn del producto' ,img:'assets/img/producto3.jpg'},
+  producto = [{numero:'1',titulo:'Producto 1', descripcion:'Descripiòn del producto',img:'assets/img/producto1.jpg', costo:100 },
+  {numero:'2',titulo:'Producto 2', descripcion:'Descripiòn del producto', img:'assets/img/producto2.jpg' ,costo:100},
+  {numero:'3',titulo:'Producto 3', descripcion:'Descripiòn del producto' ,img:'assets/img/producto3.jpg',costo:100 },
   ];
   constructor() { }
 
